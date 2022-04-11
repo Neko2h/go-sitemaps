e1bf2718de33ffa62028ee001528d0534de55dca
Neko2h <neko2h@gmail.com>
2022-04-11 23:19:29 +0300

    added lazy loading

--- sitemaps.go
+++ sitemaps.go
@@ -3,10 +3,17 @@ package sitemaps
 import (
 	"compress/gzip"
 	"context"
+	"crypto/tls"
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
 	"time"
+)
+
+var (
+	ResultChan = make(chan []Entity)
+	JobsCount  int
+	Timeout    int
 )
 
 type Sitemapindex struct {
@@ -20,59 +27,53 @@ type UrlSet struct {
 }
 
 type Entity struct {
-	Loc        string `xml:"loc"`
-	Lastmod    string `xml:"lastmod,omitempty"`
-	ChangeFreq string `xml:"changefreq,omitempty"`
+	Loc        string  `xml:"loc"`
+	Lastmod    string  `xml:"lastmod,omitempty"`
+	ChangeFreq string  `xml:"changefreq,omitempty"`
+	Images     []Image `xml:"image"`
+	Videos     []Video `xml:"video"`
 }
 
-func (s *Sitemapindex) Count() int {
-	return len(s.Entites)
+type Image struct {
+	Loc     string `xml:"loc"`
+	Title   string `xml:"title"`
+	Caption string `xml:"image:caption"`
+	Geo     string `xml:"geo_location"`
+	License string `xml:"license"`
 }
 
-func (s *UrlSet) Count() int {
-	return len(s.Entites)
+type Video struct {
+	ThumbnailLoc          string `xml:"thumbnail_loc"`
+	Title                 string `xml:"title"`
+	Description           string `xml:"description"`
+	Content_loc           string `xml:"content_loc"`
+	Player_loc            string `xml:"player_loc"`
+	Duration              string `xml:"duration"`
+	Expiration_date       string `xml:"expiration_date"`
+	Rating                string `xml:"rating"`
+	View_count            string `xml:"view_count"`
+	Publication_date      string `xml:"publication_date"`
+	Family_friendly       string `xml:"family_friendly"`
+	Requires_subscription string `xml:"requires_subscription"`
+	Live                  string `xml:"live"`
 }
 
-func worker(id int, jobs <-chan string, results chan<- []Entity, timeout int) {
-	for j := range jobs {
-		urlset, _ := ParseSitemap(j, timeout)
-		results <- urlset.Entites
+func makeRequest(url string) ([]byte, error, int) {
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-}
+	client := &http.Client{Transport: tr}
 
-func (s *Sitemapindex) GetUrls(workers int, timeout int) (int, []Entity) {
-
-	var urls []Entity
-	numJobs := len(s.Entites)
-	jobs := make(chan string, numJobs)
-	results := make(chan []Entity, numJobs)
-
-	for w := 1; w <= workers; w++ {
-		go worker(w, jobs, results, timeout)
-	}
-
-	for j := 0; j < len(s.Entites); j++ {
-		jobs <- s.Entites[j].Loc
-	}
-
-	close(jobs)
-
-	for a := 1; a <= numJobs; a++ {
-		urls = append(urls, <-results...)
-	}
-
-	close(results)
-	return len(urls), urls
-}
-
-func makeRequest(url string, timeout int) ([]byte, error, int) {
-
-	ctx, cncl := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second*time.Duration(Timeout))
 	defer cncl()
 
 	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, err, resp.StatusCode
@@ -108,7 +109,8 @@ func makeRequest(url string, timeout int) ([]byte, error, int) {
 }
 
 func ParseIndex(url string, timeout int) (Sitemapindex, int, error) {
-	body, err, status := makeRequest(url, timeout)
+	Timeout = timeout
+	body, err, status := makeRequest(url)
 	if err != nil {
 		return Sitemapindex{}, 0, err
 	}
@@ -124,9 +126,10 @@ func ParseIndex(url string, timeout int) (Sitemapindex, int, error) {
 }
 
 func ParseSitemap(url string, timeout int) (UrlSet, error) {
+	Timeout = timeout
 
 	var res UrlSet
-	body, err, status := makeRequest(url, timeout)
+	body, err, status := makeRequest(url)
 	if err != nil {
 		return UrlSet{}, err
 	}
@@ -136,4 +139,58 @@ func ParseSitemap(url string, timeout int) (UrlSet, error) {
 		return UrlSet{}, err
 	}
 	return res, nil
+}
+
+func (s *Sitemapindex) Count() int {
+	return len(s.Entites)
+}
+
+func (s *UrlSet) Count() int {
+	return len(s.Entites)
+}
+
+func worker(id int, jobs <-chan string, timeout int) {
+	for j := range jobs {
+		urlset, _ := ParseSitemap(j, timeout)
+		ResultChan <- urlset.Entites
+	}
+}
+
+func (s *Sitemapindex) GetUrls(workers int, timeout int) (int, []Entity) {
+
+	var urls []Entity
+	JobsCount = len(s.Entites)
+	jobs := make(chan string, JobsCount)
+
+	for w := 1; w <= workers; w++ {
+		go worker(w, jobs, timeout)
+	}
+
+	for j := 0; j < len(s.Entites); j++ {
+		jobs <- s.Entites[j].Loc
+	}
+
+	close(jobs)
+
+	for a := 1; a <= JobsCount; a++ {
+		urls = append(urls, <-ResultChan...)
+	}
+
+	close(ResultChan)
+	return len(urls), urls
+}
+
+func (s *Sitemapindex) GetUrlsLazy(workers int, timeout int) {
+
+	JobsCount = len(s.Entites)
+	jobs := make(chan string, JobsCount)
+
+	for w := 1; w <= workers; w++ {
+		go worker(w, jobs, timeout)
+	}
+	for j := 0; j < len(s.Entites); j++ {
+		jobs <- s.Entites[j].Loc
+	}
+
+	close(jobs)
 }
